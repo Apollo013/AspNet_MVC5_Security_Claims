99f8d1cff709334e6fd1915037df567b194b2f6f
Paul <paul.j.millar.1971@gmail.com>
2016-10-21 11:29:24 +0100

    Initial Commit

---
+++ ClaimsTransformation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClaimsTransformation.Startup))]
+namespace ClaimsTransformation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
